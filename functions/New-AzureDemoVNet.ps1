d29ce61c9b74881c130c5eb892ddc03d91d387c1
Martin Therkelsen <mth@conecto.dk>
2019-09-15 12:32:05 +0200

    Removed empty lines

--- functions/New-AzureDemoVNet.ps1
+++ functions/New-AzureDemoVNet.ps1
@@ -3,4 +3,3 @@ $Config = Get-Content -Raw -Path /Users/martin/GitForVLog/AzureDemo/Config.json 
 Foreach ($VNet in ($($Config.VirtualNetwork))) {
     New-AzVirtualNetwork -Name $VNet.Name -ResourceGroupName $VNet.ResourceGroupName -Location $Config.Location    -AddressPrefix $VNet.AddressPrefix 
 }
-
